2cd1a98382ebf226c066ef9961e36ef482a8a7a6
Punkbooster <2morrow2late@rambler.ru>
2016-01-24 00:53:33 +0100

    Complete logo effect

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -3,7 +3,7 @@ $(window).scroll(function() {
 	var wScroll = $(this).scrollTop();
 	
 	$('.logo').css({
-	'transform' : 'translate(0px, 200px)' 
+	'transform' : 'translate(0px, '+ wScroll / 2 +'%)' 
 	})
 
 })
