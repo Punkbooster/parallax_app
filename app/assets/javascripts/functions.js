661230352f0a947c0e3939570d0e116421077b73
Punkbooster <2morrow2late@rambler.ru>
2016-01-24 01:02:47 +0100

    Complete back-bird & fore-bird js effects

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -3,7 +3,15 @@ $(window).scroll(function() {
 	var wScroll = $(this).scrollTop();
 	
 	$('.logo').css({
-	'transform' : 'translate(0px, '+ wScroll / 2 +'%)' 
+		'transform' : 'translate(0px, '+ wScroll / 2 +'%)' //make logo to scroll 2 times slower
+	})
+
+	$('.back-bird').css({
+		'transform' : 'translate(0px, '+ wScroll / 4 +'%)' //make back-bird to scroll 4 times slower
+	})
+
+	$('.fore-bird').css({
+		'transform' : 'translate(0px, -'+ wScroll / 40 +'%)' //make fore-bird to scroll up, 4 times slower
 	})
 
 })
