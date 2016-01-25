9afb3ce04cec3eb7261254a5216b01bd9ee94032
Punkbooster <2morrow2late@rambler.ru>
2016-01-25 22:22:10 +0100

    Add class to figure if scrolled

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -4,18 +4,21 @@ $(window).scroll(function() {
 	
 	$('.logo').css({
 		'transform' : 'translate(0px, '+ wScroll / 2 +'%)' //make logo to scroll 2 times slower
-	})
+	});
 
 	$('.back-bird').css({
 		'transform' : 'translate(0px, '+ wScroll / 4 +'%)' //make back-bird to scroll 4 times slower
-	})
+	});
 
 	$('.fore-bird').css({
 		'transform' : 'translate(0px, -'+ wScroll / 40 +'%)' //make fore-bird to scroll up, 4 times slower
-	})
+	});
 
 	if (wScroll > $('.clothes-pics').offset().top) {
-		console.log("Hi!")
+		$('.clothes-pics figure').each(function() {
+
+			$('.clothes-pics figure').addClass('.is-showing');
+		});
 	}
 
 })
